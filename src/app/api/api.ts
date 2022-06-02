a6ce469215ee9dd47dbac49293a6fb65c8e28d78
Sergey <Sergey.rimsha.ru@gmail.com>
2022-06-02 21:35:26 +0300

    fix api baseURL

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -33,7 +33,9 @@ export type RegistrationResponseType = {
 
 const instance = axios.create({
 	// process.env.REACT_APP_BACK_URL || для gh-page
-	baseURL: 'http://localhost:7542/2.0/',
+	// https://neko-back.herokuapp.com/2.0/ для gh-page
+	// http://localhost:7542/2.0/
+	baseURL: 'https://neko-back.herokuapp.com/2.0/',
 	withCredentials: true,
 });
 
