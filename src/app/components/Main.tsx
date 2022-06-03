aef366e1157688f74b584ea03e23ed4cec83e977
pv18 <pv9998286620@yandex.ru>
2022-06-03 23:12:08 +0300

    Added Main bg

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Routing} from './Routing/Routing';
 import {HeaderContainer} from './header/HeaderContainer';
+import s from './Main.module.scss';
 
 export const Main = () => {
     return (
-        <>
+        <div className={s.wrapper}>
             <HeaderContainer/>
             <Routing/>
-        </>
+        </div>
     );
 };
 
