185ae3e88c8617a15623bfbf29fab9ff52086b90
Sergey <Sergey.rimsha.ru@gmail.com>
2022-06-04 08:25:48 +0300

    add Preloader - Main components

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
+
+import {useSelector} from 'react-redux';
+
+import {AppRootStateType} from '../store/store';
+
 import {Routing} from './Routing/Routing';
 import {HeaderContainer} from './header/HeaderContainer';
 import s from './Main.module.scss';
+import {Preloader} from './Preloader/Preloader';
 
 export const Main = () => {
+
+    const isLoading = useSelector<AppRootStateType, boolean>(state => state.app.isLoading);
+
     return (
         <div className={s.wrapper}>
-            <HeaderContainer/>
-            <Routing/>
+            <div>
+                <HeaderContainer/>
+            </div>
+            <div>
+                {isLoading && <Preloader/>}
+            </div>
+            <div>
+                <Routing/>
+            </div>
         </div>
     );
 };
--- src/app/store/reducers/appReducer.ts
+++ src/app/store/reducers/appReducer.ts
@@ -1,24 +1,32 @@
 
 type AppStateType = {
 	isAuth: boolean
+	isLoading: boolean
 }
 
 
 const initialState: AppStateType = {
-	isAuth: false,	
+	isAuth: false,
+	isLoading: false,
 };
 
 
-export type AppActionsType = ReturnType<typeof setIsAuth> 
+export type AppActionsType = ReturnType<typeof setIsAuth> | ReturnType<typeof setIsLoading>
 
 
 
-export const appReducer = (state = initialState, action: AppActionsType) => {
+export const appReducer = (state = initialState, action: AppActionsType):AppStateType => {
 	switch (action.type) {
 		case 'APP/SET-IS-AUTH': {
 			return {
 				...state,
 				isAuth: action.isAuth,
+			};
+		}
+		case 'APP/SET-IS-LOADING': {
+			return {
+				...state,
+				isLoading: action.isLoading,
 			};
 		}
 		default: return state;
@@ -31,5 +39,12 @@ export const setIsAuth = (isAuth: boolean) => {
 	return {
 		type: 'APP/SET-IS-AUTH',
 		isAuth,
+	} as const;
+};
+
+export const setIsLoading = (isLoading: boolean) => {
+	return {
+		type: 'APP/SET-IS-LOADING',
+		isLoading,
 	} as const;
 };
