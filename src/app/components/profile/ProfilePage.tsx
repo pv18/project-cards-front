8556cb6387318b9b142ff0518d269f776732e48c
pv18 <pv9998286620@yandex.ru>
2022-06-08 16:42:57 +0300

    reference was changed

--- src/app/components/profile/ProfilePage.tsx
+++ src/app/components/profile/ProfilePage.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 import s from './ProfilePage.module.scss';
 import ProfileContainer from './ProfileContainer';
-import DoubleSlider from "../DoubleSlider/DoubleSlider";
 
 export const ProfilePage = () => {
     return (
         <div className={s.block}>
             <div className={s.container}>
                 <div><ProfileContainer/></div>
-                <div className={s.sidebar__numberOfCards}>{<DoubleSlider/>/*<NumberOfCards/>*/}</div>
+                <div className={s.sidebar__numberOfCards}>{/*<NumberOfCards/>*/}</div>
                 <div className={s.myPacksList}>{/*<MyPacksList/>*/}</div>
             </div>
         </div>
