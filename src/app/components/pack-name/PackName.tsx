004fd73c1fdf0ddb9b02ffc59185f4027114d30f
pv18 <pv9998286620@yandex.ru>
2022-06-12 12:59:06 +0300

    change styles

--- src/app/components/pack-name/PackName.tsx
+++ src/app/components/pack-name/PackName.tsx
@@ -2,20 +2,22 @@ import React, {useEffect, useState} from 'react';
 
 import {useNavigate, useParams} from 'react-router-dom';
 
+import {useDispatch, useSelector} from 'react-redux';
+
 import Arrow from '../../assets/img/arrow-left.svg';
 
 import {TablePackNameContainer} from '../../../features/f2-PackName/TablePackNameContainer';
 
-import s from './PackName.module.scss';
 import {TableSearch} from '../TableSearch/TableSearch';
 import {apiCard} from '../../../features/f2-PackName/api/api';
 import {CardPackNameType, setPackNameList} from '../../../features/f2-PackName/api/bll/packNameReducer';
-import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from '../../store/store';
 import {Button} from '../Button/Button';
 
+import s from './PackName.module.scss';
+
 export const PackName = () => {
-    const [search, setSearch] = useState<string>('')
+    const [search, setSearch] = useState<string>('');
     const navigate = useNavigate();
     const cards = useSelector<AppRootStateType, CardPackNameType[]>(state => state.packName.cards);
     const dispatch = useDispatch();
@@ -34,19 +36,21 @@ export const PackName = () => {
 
     const getFilteredCardsAfterSearch = search === ''
         ? cards
-        : cards.filter(c => c.question.toLowerCase().includes(search.toLowerCase()))
+        : cards.filter(c => c.question.toLowerCase().includes(search.toLowerCase()));
 
     return (
         <div className={s.wrapper}>
             <div>ЗДЕСЬ БУДЕТ HEADER</div>
             <div className={s.table}>
                 <h2 className={s.title}>
-                    <img onClick={() => navigate(-1)} src={Arrow} alt="arrow left"/>
+                    <img onClick={() => navigate(-1)} src={Arrow} alt='arrow left'/>
                     <span>Pack Name "{name}"</span>
                 </h2>
                 <div className={s.search}>
                     <TableSearch title={search} changeTitle={setSearch}/>
-                    <Button width={'184px'}>Add new pack</Button>
+                    <div className={s.buttonWrap}>
+                        <Button width={'184px'}>Add new pack</Button>
+                    </div>
                 </div>
                 <TablePackNameContainer data={getFilteredCardsAfterSearch}/>
             </div>
