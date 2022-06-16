8c10f5c026358e16420addb38ad0e7f83f03e6b8
pv18 <pv9998286620@yandex.ru>
2022-06-16 22:38:08 +0300

    changed Modal Delete.

--- src/app/components/packs-list/PacksList.tsx
+++ src/app/components/packs-list/PacksList.tsx
@@ -1,27 +1,14 @@
-import React, {useState} from 'react';
-
-import {useDispatch, useSelector} from 'react-redux';
+import React from 'react';
 
 import {TablePacksContainer} from '../tablePack/TablePacksContainer';
 import PaginationContainer from '../Pagination/PaginationContainer';
 
 import MyAll from '../my-all/MyAll';
-import {Modal} from '../Modal/Modal';
 import {NavBarContainer} from '../navbar/NavBarContainer';
-import {Button} from '../Button/Button';
-
-import {AppRootStateType} from '../../store/store';
-
-import {changeModalADD, changeModalDelete} from '../../store/reducers/modalsReducer';
 
 import s from './PacksList.module.scss';
-import {ModalDelete} from './modals/deleteModal/ModalDelete';
-import {ModalAdd} from './modals/addModal/ModalAdd';
 
 export const PacksList = () => {
-    const isDelete = useSelector<AppRootStateType, boolean>(state => state.modals.modalDelete);
-    const isADD = useSelector<AppRootStateType, boolean>(state => state.modals.modalADD);
-    const dispatch = useDispatch();
 
     return (
         <div className={s.wrapper}>
