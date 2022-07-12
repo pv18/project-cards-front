832390a64369ab1cf9065d9d8e9b98ffe9e65ec7
ElenaHrudavenka <kisseli@mail.ru>
2022-07-12 19:36:21 +0300

    Profile was changed

---
+++ src/components/c7-DoubleRange/DoubleRange.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DoubleRange = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DoubleRange;
--- src/features/f4-profile/ProfileContainer.tsx
+++ src/features/f4-profile/ProfileContainer.tsx
@@ -7,8 +7,10 @@ import {useSelector} from 'react-redux';
 import {PATH} from '../../app/Routing/Routing';
 import {AppRootStateType} from '../../store/store';
 
-import Profile from './Profile';
+import {NavBarContainer} from '../f6-navbar/NavBarContainer';
 
+import Profile from './Profile';
+import s from './ProfileContainer.module.scss'
 
 const ProfileContainer = () => {
 
@@ -16,20 +18,21 @@ const ProfileContainer = () => {
     const email = useSelector<AppRootStateType, string>(state => state.profile.userData.email);
     const name = useSelector<AppRootStateType, string>(state => state.profile.userData.name);
 
-    const isAuth = useSelector<AppRootStateType, boolean>(state => state.app.isAuth);
-
     // обработчик для кнопки редактирования профиля
     const onClickHandler = () => {
         navigate(PATH.EDIT_PROFILE);
     };
 
     return (
-        <div>
-            <Profile onClickEditHandler={onClickHandler}
-                     email={email}
-                     name={name}
-            />
-        </div>
+        <>
+            <NavBarContainer/>
+            <div className={s.blockProfile}>
+                <Profile onClickEditHandler={onClickHandler}
+                         email={email}
+                         name={name}
+                />
+            </div>
+        </>
     );
 };
 
