f63c54893180345028e1a81bdef2a08216076bc6
pv18 <pv9998286620@yandex.ru>
2022-07-15 20:07:16 +0300

    change url

--- src/features/f1-packsList/api/api.ts
+++ src/features/f1-packsList/api/api.ts
@@ -23,8 +23,8 @@ const instance = axios.create({
 	// process.env.REACT_APP_BACK_URL || для gh-page
 	// https://neko-back.herokuapp.com/2.0/ для gh-page
 	// http://localhost:7542/2.0/
-	baseURL: 'http://localhost:7542/2.0/',
-	// baseURL: 'https://neko-back.herokuapp.com/2.0/',
+	// baseURL: 'http://localhost:7542/2.0/',
+	baseURL: 'https://neko-back.herokuapp.com/2.0/',
 	withCredentials: true,
 });
 
